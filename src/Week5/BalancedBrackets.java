72cb2e2f9062f422496d6f98a4ffc1f1a0aaadc4
tanhtanh1505 <tanhtanh1505@gmail.com>
2021-10-06 17:48:21 +0700

    update

--- src/Week5/BalancedBrackets.java
+++ src/Week5/BalancedBrackets.java
@@ -4,16 +4,31 @@ import java.util.Scanner;
 
 public class BalancedBrackets {
     public static String isBalanced(String s) {
-        Stack<Character> st = new Stack<Character>();
+        Stack<Character> st = new Stack<>();
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '('){
                 st.push(s.charAt(i));
             }
             else {
-                if(s.charAt(i) == st.peek()){
-                    st.pop();
+                if(st.isEmpty()){
+                    return "NO";
                 }
-                else return "NO";
+                else {
+                    switch (s.charAt(i)){
+                        case '}':
+                            if(st.pop() != '{')
+                                return "NO";
+                            break;
+                        case ']':
+                            if(st.pop() != '[')
+                                return "NO";
+                            break;
+                        case ')':
+                            if(st.pop() != '(')
+                                return "NO";
+                            break;
+                    }
+                }
             }
         }
         if(st.isEmpty()){
