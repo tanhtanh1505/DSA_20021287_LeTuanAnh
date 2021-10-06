91063cbce904d137ddc3c744ab04d8661e38615b
tanhtanh1505 <tanhtanh1505@gmail.com>
2021-10-06 15:11:45 +0700

    exw5

--- src/Week5/BalancedBrackets.java
+++ src/Week5/BalancedBrackets.java
@@ -1,2 +1,30 @@
-package Week5;public class BalancedBrackets {
+package Week5;
+
+import java.util.Scanner;
+
+public class BalancedBrackets {
+    public static String isBalanced(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '('){
+                st.push(s.charAt(i));
+            }
+            else {
+                if(s.charAt(i) == st.peek()){
+                    st.pop();
+                }
+                else return "NO";
+            }
+        }
+        if(st.isEmpty()){
+            return "YES";
+        }
+        else return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(isBalanced(s));
+    }
 }
