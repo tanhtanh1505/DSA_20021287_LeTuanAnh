3b3073d6ec31e87894c806b348a5ed21c54c5d78
tanhtanh1505 <tanhtanh1505@gmail.com>
2021-10-14 16:16:52 +0700

    up ex6

--- src/Week5/EqualStack.java
+++ src/Week5/EqualStack.java
@@ -3,28 +3,56 @@ package Week5;
 import java.util.List;
 
 public class EqualStack {
-    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
-        h1.add(0);
-        h2.add(0);
-        h3.add(0);
-        for(int i = h1.size() - 2; i >= 0 ; i--){
-            h1.set(i, h1.get(i) + h1.get(i+1));
+    public static int FindMax(Stack<Integer> first, Stack<Integer> second, Stack<Integer> third)
+    {
+        int a = first.pop();
+        int b = second.pop();
+        int c = third.pop();
+        first.push(a);
+        second.push(b);
+        third.push(c);
+        if (a == b && a == c) return 3;
+        if (a >= b && a >= c) return 0;
+        else if (b >= a && b >= c) return 1;
+        return 2;
+    }
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3)
+    {
+        Stack<Integer> First = new Stack<>();
+        First.push(0);
+        Stack<Integer> Second = new Stack<>();
+        Second.push(0);
+        Stack<Integer> Third = new Stack<>();
+        Third.push(0);
+
+        int sum = 0;
+        for (int i = h1.size() - 1;i >= 0;i--)
+        {
+            sum += h1.get(i);
+            First.push(sum);
         }
-        for(int i = h2.size() - 2; i >= 0 ; i--){
-            h2.set(i, h2.get(i) + h2.get(i+1));
+
+        sum = 0;
+        for (int i = h2.size() - 1;i >= 0;i--)
+        {
+            sum += h2.get(i);
+            Second.push(sum);
         }
-        for(int i = h3.size() - 2; i >= 0 ; i--){
-            h3.set(i, h3.get(i) + h3.get(i+1));
+
+        sum = 0;
+        for (int i = h3.size() - 1;i >= 0;i--)
+        {
+            sum += h3.get(i);
+            Third.push(sum);
         }
-        int x1 = 0, x2 = 0, x3 = 0;
-        while(!(h1.get(x1) == h2.get(x2) && h1.get(x1) == h3.get(x3))){
-            if(h1.get(x1) > h2.get(x2) || h1.get(x1) > h3.get(x3))
-                x1++;
-            else if(h2.get(x2) > h1.get(x1) || h2.get(x2) > h3.get(x3))
-                x2++;
-            else if(h3.get(x3) > h1.get(x1) || h3.get(x3) > h2.get(x2))
-                x3++;
+
+        while (true)
+        {
+            int c = FindMax(First,Second,Third);
+            if (c == 3) return First.pop();
+            else if (c == 0) First.pop();
+            else if (c == 1) Second.pop();
+            else if (c == 2) Third.pop();
         }
-        return h1.get(x1);
     }
 }
